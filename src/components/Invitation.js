4b094a4a69e4712401eed7ae192c532cac3e4878
Hoon Lee <yl.hoony@gmail.com>
2018-06-04 19:23:27 -0700

    implemented

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,13 @@
-// Code Invitation Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Invitation extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>You've been invited!</h1>
+        {this.props.children}
+      </div>
+    )
+  }
+}
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,18 @@
-// Code ThemedDecoration Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class ThemedDecorations extends React.Component {
+  render() {
+    const childrenWithClass = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        className: this.props.theme
+      })
+    })
+
+    return (
+      <div>
+        {childrenWithClass}
+      </div>
+    )
+  }
+}
